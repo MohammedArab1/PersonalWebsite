21ca7f5ca810636ee77141f241e3cc3cdea78178
Mohammed Arab <arab0033@algonquinlive.com>
2023-02-25 12:01:32 -0500

    adding new classes, working with creating components for footer and maybe header. This commit is a placeholder only

---
+++ JavaScript/FooterJS.js
@@ -0,0 +1,11 @@
+const root = document.querySelector('.root')
+
+// export const Footer = () => {
+//   const footerDev = document.getElementById("footer").appendChild(document.createElement("footer").classList.add("row py-3 my-3 border-top"))
+  
+  
+// }
+
+root.appendChild(document.createElement("div").classList.add('container tf3')
+  .appendChild((document.createElement('footer'.classList.add('row py-3 my-3 border-top'))))
+)
